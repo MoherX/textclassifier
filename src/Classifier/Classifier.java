58b4bb67114dd5911e309a5137f877bd571a823e
aacalfa <aacalfa@gmail.com>
2015-03-02 22:04:31 -0800

    Added entertainment data set for testing.

--- src/Classifier/Classifier.java
+++ src/Classifier/Classifier.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import FeatureSelection.Porter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -172,6 +173,11 @@ public class Classifier {
 	return scores;
   }
 
+  /**
+   * Classifies a document
+   * @param input1 string with the document contents
+   * @return 
+   */
   public String classify(String input1) {
 
 	input = preprocess(input1);
@@ -260,6 +266,12 @@ public class Classifier {
 
   }
 
+  /**
+   * Classifies the 20 newsgroups data set
+   * @param clsf classifier object
+   * @throws FileNotFoundException
+   * @throws IOException 
+   */
   public static void classify20Newsgroups(Classifier clsf) throws FileNotFoundException, IOException {
 	double politicsCorrect = 0; // Number of correct classifications for politics
 	double sportsCorrect = 0; // Number of correct classifications for sports
@@ -329,6 +341,12 @@ public class Classifier {
 	out.close();
   }
 
+  /**
+   * Classifies the Reuters data set
+   * @param clsf classifier object
+   * @throws FileNotFoundException
+   * @throws IOException 
+   */
   public static void classifyReuters(Classifier clsf) throws FileNotFoundException, IOException {
 	double businessCorrect = 0; // Number of correct classifications for business
 	double businessDocs = 0;
@@ -358,10 +376,69 @@ public class Classifier {
 		clsf.numDocs++;
 		businessDocs++;
 	  }
-	  double accuracy = clsf.correct / clsf.numDocs;
 	  System.out.println("Classifier business accuracy = " + businessCorrect / businessDocs);
-	  System.out.println("Classifier total accuracy = " + accuracy);
 	}
+  }
+
+  /**
+   * Get all files from a directory
+   * @param dir directory to be searched
+   * @return list containing the name of all files in dir.
+   */
+  public static ArrayList<String> readFiles(File dir) {
+	File[] fs = dir.listFiles();
+	ArrayList<String> files = new ArrayList<>();
+	for (int i = 0; i < fs.length; i++) {
+	  files.add(fs[i].getAbsolutePath());
+	  if (fs[i].isDirectory()) {
+		try {
+		  readFiles(fs[i]);
+		} catch (Exception e) {
+		}
+	  }
+	}
+	return files;
+  }
+  
+/**
+ * Classifies the MSN entertainment data set
+ * @param clsf classifier object
+ * @throws FileNotFoundException
+ * @throws IOException 
+ */
+  public static void classifyMSN(Classifier clsf) throws FileNotFoundException, IOException {
+	double entertainmentCorrect = 0; // Number of correct classifications for entertainment
+	double entertainmentDocs = 0;
+
+	// Get all files in the MSN dataset directory
+	File dir = new File("entertainment.msn.com");
+	ArrayList<String> files = readFiles(dir);
+
+	// Read the MSN dataset
+	for (String file : files) {
+	  String fileContents = null;
+	  try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+		// Gather all content from a file
+		for (String line; (line = br.readLine()) != null;) {
+		  // process the line.
+		  fileContents += line;
+		}
+		// Get classifier result
+		String result = clsf.classify(fileContents);
+
+		// Compare classifier result with dataset actual category
+		if (result.equals("entertainment")) { // Correct classification
+		  entertainmentCorrect++;
+		  clsf.correct++;
+		}
+		clsf.numDocs++;
+		entertainmentDocs++;
+	  }
+	}
+	double accuracy = clsf.correct / clsf.numDocs;
+	System.out.println("Classifier entertainment accuracy = " + entertainmentCorrect / entertainmentDocs);
+	System.out.println("Classifier total accuracy = " + accuracy);
+
   }
 
   public static void main(String args[]) throws IOException {
@@ -370,8 +447,10 @@ public class Classifier {
 	try {
 	  classify20Newsgroups(clsf);
 	  classifyReuters(clsf);
+	  classifyMSN(clsf);
 	} catch (IOException ex) {
-	  Logger.getLogger(Classifier.class.getName()).log(Level.SEVERE, null, ex);
+	  Logger.getLogger(Classifier.class
+			  .getName()).log(Level.SEVERE, null, ex);
 	}
 
   }
